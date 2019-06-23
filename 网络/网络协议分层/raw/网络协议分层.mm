7677e11ddc052c50b81c425ed7a12283ec1a5e87
martin <838379742@qq.com>
2019-06-24 07:20:17 +0800

    添加TCP建立连接

---
+++ 网络/网络协议分层/raw/网络协议分层.mm
@@ -0,0 +1,23 @@
+<map>
+		<node ID="root" TEXT="网络协议分层">		<node TEXT="物理层" ID="16316b84a47e83126" STYLE="bubble" POSITION="right">
+		<node TEXT="通过物理手段解决连接问题" ID="7316b84a5d59f17" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="数据链路层" ID="39a16b84a48a26176" STYLE="bubble" POSITION="right">
+		<node TEXT="通过MAC地址解决点对点通信问题" ID="14516b84a5985000b" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="网络层" ID="25e16b84a49c21004" STYLE="bubble" POSITION="right">
+		<node TEXT="通过IP协议解决不同局域网的通信问题" ID="3b616b84a5414a0f" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="传输层" ID="31e16b84a4a99b103" STYLE="bubble" POSITION="left">
+		<node TEXT="解决端到端的通信问题" ID="17516b84a520ec0c3" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="应用层" ID="18f16b84a5193a0bc" STYLE="bubble" POSITION="left">
+		<node TEXT="解决不同应用的通信协议" ID="2ef16b84a4b47e105" STYLE="fork">
+		</node>
+		</node>
+</node>
+</map>
